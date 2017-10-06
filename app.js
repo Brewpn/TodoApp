f540ba806f03bbf6d0addf57985e7852ced59e49
Brewpn <brewpn@gmail.com>
2017-10-06 17:23:18 +0300

    add passport-google auth

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('./lib/mongoose');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+
+const users = require('./routes/users');
 
 var app = express();
 
+require('./routes/index')(app, passport);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -21,9 +26,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: "topSecret",
+    key: "sid",
+    cookie: {
+        path: "/",
+        httpOnly: true,
+        maxAge: null
+    },
+    store: new MongoStore({
+        mongooseConnection: mongoose.connection,
+        adapter: 'connect-mongo'
+    }),
+    resave: true,
+    saveUninitialized: true
+}));
 
-app.use('/', index);
-app.use('/users', users);
+app.use(passport.initialize());
+app.use(passport.session());
+require('./lib/passport')(passport);
+// app.use('/', index);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,39 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = function (app, passport) {
 
-module.exports = router;
+  app.get('/', require('./frontpage').get);
+
+  app.get('/task_list', isLoggedIn, function(req, res) {
+      res.render('task_list.ejs', {
+          user : req.user // get the user out of session and pass to template
+      });
+  });
+
+  app.get('/logout', function(req, res) {
+      req.logout();
+      res.redirect('/');
+  });
+
+
+  //google auth link
+  app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+  //callback google auth
+  app.get('/auth/google/callback',
+      passport.authenticate('google', {
+          successRedirect : '/task_list',
+          failureRedirect : '/'
+      })
+  );
+};
+
+function isLoggedIn(req, res, next) {
+
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated())
+        return next();
+
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
