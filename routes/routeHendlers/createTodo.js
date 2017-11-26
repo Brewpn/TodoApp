6240e42ac27ad97ca378eeecc97b5ecaa8fb01ff
Brewpn <brewpn@gmail.com>
2017-11-26 18:29:57 +0200

    Created TodoFolder collection, changed create, delete, get routes.

--- routes/routeHendlers/createTodo.js
+++ routes/routeHendlers/createTodo.js
@@ -4,28 +4,29 @@ const ObjectId = mongoose.Types.ObjectId;
 
 exports.post = function (req, res, done) {
 
-    // const newTodo = req.body;
-    //
-    // TodoFolder.findOne({_id : newTodo.folderId})
-    //     .exec()
-    //     .then( todoFolder => {
-    //         if (!todoFolder.todoList)
-    //             todoFolder.todoList = {};
-    //
-    //         todoFolder.todoList.push({
-    //             _id : new ObjectId(),
-    //             todoDescription : newTodo.todoDescription
-    //         });
-    //
-    //         return todoFolder;
-    //     })
-    //     .then( todoFolder => {
-    //         todoFolder.save(err => {
-    //             if (err)
-    //                 throw err;
-    //         })
-    //     })
-    //     .catch(err => {
-    //         return done(err)
-    //     });
+    const newTodo = req.body;
+
+    TodoFolder.findOne({_id : newTodo.folderId})
+        .exec()
+        .then( todoFolder => {
+            if (!todoFolder.todoList)
+                todoFolder.todoList = {};
+
+            todoFolder.todoList.push({
+                _id : new ObjectId(),
+                todoDescription : newTodo.todoDescription
+            });
+
+            return todoFolder;
+        })
+        .then( todoFolder => {
+            todoFolder.save(err => {
+                if (err)
+                    throw err;
+                res.redirect('/todoListOut')
+            })
+        })
+        .catch(err => {
+            return done(err)
+        });
 };
